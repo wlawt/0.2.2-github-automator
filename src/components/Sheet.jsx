d010e2296ec09e20deae6ed0c45b8c8055958bff
Will L <triwnlproductions@gmail.com>
2020-06-02 15:46:10 -0400

    wlawt/initial structure

---
+++ src/components/Sheet.jsx
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react'
+
+class Sheet extends Component {
+    render() {
+        return (
+            <Fragment>
+                <h1>hello</h1>
+            </Fragment>
+        )
+    }
+}
+
+export default Sheet
